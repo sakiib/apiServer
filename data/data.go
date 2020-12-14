f344406f7c1b6cda76c40238293afbb182b791ef
sakibalamin <sakibalamin@appscode.com>
2020-12-14 12:01:23 +0600

    added user data in model branch

---
+++ data/data.go
@@ -0,0 +1,10 @@
+package data
+
+import "apiServer/model"
+
+var Users = []model.User{
+	model.User{ID: "1", FirstName: "sakib", LastName: "alamin"},
+	model.User{ID: "2", FirstName: "prangon", LastName: "majumder"},
+	model.User{ID: "3", FirstName: "mehedi", LastName: "hasan"},
+	model.User{ID: "4", FirstName: "sahadat", LastName: "hossain"},
+}
