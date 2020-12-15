a9f84329d42c48b521ec76eff759f1f6889a0f40
sakibalamin <sakibalamin@appscode.com>
2020-12-15 10:30:57 +0600

    status code updated for proper response

--- api/api.go
+++ api/api.go
@@ -18,6 +18,9 @@ var credential struct {
 
 var isAuthRequired bool
 
+//@route GET /api/users
+//@desc Gets all the available users
+
 func getUsers(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
 	fmt.Println("getUsers")
@@ -31,8 +34,13 @@ func getUsers(response http.ResponseWriter, request *http.Request) {
 	}
 	fmt.Println("Authentication successful!")
 
+	response.Write([]byte(`{"message":"Users List!"}`))
+	response.WriteHeader(http.StatusOK)
 	json.NewEncoder(response).Encode(data.Users)
 }
+
+//@route GET /api/user/id
+//@desc Gets a single user with the given id
 
 func getUser(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
@@ -50,6 +58,8 @@ func getUser(response http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
 	for _, user := range data.Users {
 		if user.ID == params["id"] {
+			response.Write([]byte(`{"message":"User Found!"}`))
+			response.WriteHeader(http.StatusOK)
 			json.NewEncoder(response).Encode(user)
 			return
 		}
@@ -57,6 +67,9 @@ func getUser(response http.ResponseWriter, request *http.Request) {
 	response.WriteHeader(http.StatusNoContent)
 	json.NewEncoder(response).Encode(model.User{})
 }
+
+//@route POST /api/user/id
+//@desc Create a new user with given info
 
 func addUser(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
@@ -71,14 +84,27 @@ func addUser(response http.ResponseWriter, request *http.Request) {
 	}
 	fmt.Println("Authentication successful!")
 
-	user := model.User{}
-	if err := json.NewDecoder(request.Body).Decode(&user); err != nil {
+	newUser := model.User{}
+	if err := json.NewDecoder(request.Body).Decode(&newUser); err != nil {
 		log.Fatal(err)
 	}
 
-	data.Users = append(data.Users, user)
+	for _, user := range data.Users {
+		if newUser.ID == user.ID {
+			response.WriteHeader(http.StatusConflict)
+			response.Write([]byte(`{"message":"User with the given ID already exists!"}`))
+			return
+		}
+	}
+
+	response.Write([]byte(`{"message":"User Created!"}`))
+	response.WriteHeader(http.StatusCreated)
+	data.Users = append(data.Users, newUser)
 	json.NewEncoder(response).Encode(data.Users)
 }
+
+//@route PUT /api/user/id
+//@desc Update a user details with given id
 
 func updateUser(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
@@ -99,15 +125,28 @@ func updateUser(response http.ResponseWriter, request *http.Request) {
 	}
 
 	params := mux.Vars(request)
+	userUpdated := false
 	for index, user := range data.Users {
 		if user.ID == params["id"] {
 			data.Users = append(data.Users[:index], data.Users[index+1:]...)
 			data.Users = append(data.Users, newUser)
+			userUpdated = true
 			break
 		}
 	}
+	if !userUpdated {
+		response.WriteHeader(http.StatusNoContent)
+		response.Write([]byte(`{"message":"User with the given ID not Found!"}`))
+		return
+	}
+
+	response.Write([]byte(`{"message":"User Updated!"}`))
+	response.WriteHeader(http.StatusCreated)
 	json.NewEncoder(response).Encode(data.Users)
 }
+
+//@route DELETE /api/user/id
+//@desc Delete an user with the given ID
 
 func deleteUser(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
@@ -122,12 +161,23 @@ func deleteUser(response http.ResponseWriter, request *http.Request) {
 	}
 	fmt.Println("Authentication successful!")
 	params := mux.Vars(request)
+	userDeleted := false
 	for index, user := range data.Users {
 		if user.ID == params["id"] {
 			data.Users = append(data.Users[:index], data.Users[index+1:]...)
+			userDeleted = true
 			break
 		}
 	}
+
+	if !userDeleted {
+		response.WriteHeader(http.StatusNoContent)
+		response.Write([]byte(`{"message":"User with the given ID not Found!"}`))
+		return
+	}
+
+	response.Write([]byte(`{"message":"User Deleted!"}`))
+	response.WriteHeader(http.StatusOK)
 	json.NewEncoder(response).Encode(data.Users)
 }
 
