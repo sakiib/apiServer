0bef4ccc1d08abd82f18d42a4b7ae562787a19f2
sakibalamin <sakibalamin@appscode.com>
2020-12-21 17:57:44 +0600

    nested data added for model

--- data/data.go
+++ data/data.go
@@ -7,44 +7,36 @@ var Users = []model.User{
 		ID:        "1",
 		FirstName: "sakib",
 		LastName:  "alamin",
-		FavouriteMovie: &model.Movie{
-			ID:     "1",
-			Title:  "Movie-1",
-			Genre:  "Comedy",
-			Rating: 5,
+		FavouriteMovies: []model.Movie{
+			model.Movie{ID: "1", Title: "movie-1", Genre: "rom-com", Rating: 5},
+			model.Movie{ID: "2", Title: "movie-2", Genre: "action", Rating: 2},
 		},
 	},
 	model.User{
 		ID:        "2",
 		FirstName: "prangon",
 		LastName:  "majumder",
-		FavouriteMovie: &model.Movie{
-			ID:     "2",
-			Title:  "Movie-2",
-			Genre:  "Sci-Fi",
-			Rating: 5,
+		FavouriteMovies: []model.Movie{
+			model.Movie{ID: "3", Title: "movie-3", Genre: "horror", Rating: 3},
+			model.Movie{ID: "4", Title: "movie-4", Genre: "action", Rating: 4},
 		},
 	},
 	model.User{
 		ID:        "3",
 		FirstName: "mehedi",
 		LastName:  "hasan",
-		FavouriteMovie: &model.Movie{
-			ID:     "3",
-			Title:  "Movie-3",
-			Genre:  "Horror",
-			Rating: 3,
+		FavouriteMovies: []model.Movie{
+			model.Movie{ID: "5", Title: "movie-5", Genre: "comedy", Rating: 5},
+			model.Movie{ID: "6", Title: "movie-6", Genre: "sci-fi", Rating: 3},
 		},
 	},
 	model.User{
 		ID:        "4",
 		FirstName: "sahadat",
 		LastName:  "hossain",
-		FavouriteMovie: &model.Movie{
-			ID:     "4",
-			Title:  "Movie-4",
-			Genre:  "Action",
-			Rating: 4,
+		FavouriteMovies: []model.Movie{
+			model.Movie{ID: "7", Title: "movie-7", Genre: "romantic", Rating: 3},
+			model.Movie{ID: "8", Title: "movie-8", Genre: "action", Rating: 2},
 		},
 	},
 }
--- model/user.go
+++ model/user.go
@@ -1,8 +1,8 @@
 package model
 
 type User struct {
-	ID             string `json:"id"`
-	FirstName      string `json:"firstname"`
-	LastName       string `json:"lastname"`
-	FavouriteMovie *Movie `json:"favouriteMovie"`
+	ID             string  `json:"id"`
+	FirstName      string  `json:"firstname"`
+	LastName       string  `json:"lastname"`
+	FavouriteMovies []Movie `json:"favouriteMovies"`
 }
